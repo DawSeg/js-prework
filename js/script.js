66b4108171efc317b6c929393fea591f68d23f3b
Dawid Segieta <buler43221@gmail.com>
2022-12-03 14:18:36 +0100

    addComputerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-let playerMove = "papier";
-
-printMessage("Zagrałem" + playerMove + "! Jeśli Twój ruch to kamień, to wygrywasz!")
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = "nieznany ruch";
+if (randomNumber == 1) {
+    computerMove= "kamień";
+}
+else if (randomNumber==2) { 
+    computerMove = "papier";
+}
+else if (randomNumber==3) {
+    computerMove= "nożyce";
+}
+printMessage("Mój ruch to: " + computerMove);
